8e283cecb35f2b27af3ba3d3ad197ea1ba18c428
Shahad Muhammed <shahadmuhammed21@gmail.com>
2024-08-05 11:26:44 +0530

    frontend design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegisterComponent } from './register/register.component';
+import { CartComponent } from './cart/cart.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { PnfComponent } from './pnf/pnf.component';
+import { AllProductsComponent } from './all-products/all-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    RegisterComponent,
+    CartComponent,
+    WishlistComponent,
+    ViewProductComponent,
+    PnfComponent,
+    AllProductsComponent
   ],
   imports: [
     BrowserModule,
